838a2e0a3b44e192fb318fa52e4121e419e95c97
Savio Moraes <smmsavio@hotmail.com>
2024-06-20 17:51:47 -0300

    feat: backend

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+
+const app = require('./app');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
